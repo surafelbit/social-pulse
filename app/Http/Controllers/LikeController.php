5cfefb28d7878f4a4d65520623947c002c0d5490
Your Real Name <surafelmuhabaw17@gmail.com>
2026-07-02 11:55:29 +0300

    like controller setup

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Like;
 use Illuminate\Http\Request;
 
 class LikeController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $request->validate(['post_id' => 'required|exists:posts,id']);
+
+        // Prevent duplicate likes
+        $request->user()->likes()->updateOrCreate(
+            ['post_id' => $request->post_id]
+        );
+
+        return back();
+    }
 }
