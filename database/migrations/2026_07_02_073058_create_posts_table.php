494424eb2bc6f4f62edda56277dfeda26cf0bb49
Your Real Name <surafelmuhabaw17@gmail.com>
2026-07-03 10:17:30 +0300

    migtation of adding profile and avatar for user

--- database/migrations/2026_07_02_073058_create_posts_table.php
+++ database/migrations/2026_07_02_073058_create_posts_table.php
@@ -13,18 +13,14 @@ return new class extends Migration
 {
     Schema::create('posts', function (Blueprint $table) {
         $table->id();
-        // This links the post to a user. It creates a user_id column.
-        $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-        $table->string('content'); // The actual post text
+        $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        $table->text('content');
         $table->timestamps();
     });
 }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('posts');
-    }
+public function down(): void
+{
+    Schema::dropIfExists('posts');
+}
 };
