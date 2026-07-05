4b4265f15f1252502977c0e03a4368489bd0f09c
Your Real Name <surafelmuhabaw17@gmail.com>
2026-07-05 17:26:31 +0300

    notification table and migration creation

--- database/migrations/2026_07_05_142323_create_notifications_table.php
+++ database/migrations/2026_07_05_142323_create_notifications_table.php
@@ -10,12 +10,16 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('notifications', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('notifications', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Who gets the notification
+        $table->string('type'); // e.g., 'like', 'follow'
+        $table->unsignedBigInteger('notifiable_id'); // ID of the post or user that caused the event
+        $table->boolean('is_read')->default(false);
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
