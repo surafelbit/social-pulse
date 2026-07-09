9ee3ea296b5aa8863f2fb80d1f6d9113886faa24
Your Real Name <surafelmuhabaw17@gmail.com>
2026-07-09 16:53:14 +0300

    fixing the broken message page displaying all friends

--- resources/js/Pages/PublicProfile.vue
+++ resources/js/Pages/PublicProfile.vue
@@ -195,6 +195,27 @@ const toggleFollowUser = (user) => {
                         >Feed</Link
                     >
                     <Link
+                        :href="route('conversations.index')"
+                        class="relative flex items-center justify-center w-8 h-8 rounded-full transition-colors hover:bg-[#32cd32]/10"
+                        aria-label="Messages"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="w-4 h-4"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            style="color: var(--sp-text-2)"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                            />
+                        </svg>
+                    </Link>
+                    <Link
                         :href="route('notifications.index')"
                         class="relative flex items-center justify-center w-8 h-8 rounded-full transition-colors hover:bg-[#32cd32]/10"
                         aria-label="Notifications"
